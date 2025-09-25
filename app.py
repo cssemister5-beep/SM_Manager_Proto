a2319069e13e59605632c3a5897e25c460334089
cssemister5-beep <cssemister5@gmail.com>
2025-09-25 21:22:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -219,7 +219,12 @@ def proxy(url):
 
 
 # --------- Startup Check ---------
+# if __name__ == "__main__":
+#     if not os.path.exists('users.json'):
+#         save_users({})
+#     app.run(debug=True)
 if __name__ == "__main__":
     if not os.path.exists('users.json'):
         save_users({})
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=10000)
+
